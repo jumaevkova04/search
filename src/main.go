377bfb3a1ec1ccea9ae6a565038316263a22743b
jumaevkova04 <jumaevkovad@gmail.com>
2020-10-26 17:24:19 +0500

    modified

--- src/main.go
+++ src/main.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jumaevkova04/search/pkg/search"
-)
-
 func main() {
 	// 	a := search.FindAllPhraseInFile("name", "src/text.txt")
 	// 	fmt.Println(a)
-	b := search.FindAnyPhraseInFile("name", "src/text.txt")
-	fmt.Println(b)
+	// b := search.FindAnyPhraseInFile("name", "src/text.txt")
+	// fmt.Println(b)
 }
